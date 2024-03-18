6f43e10166c33fde80da504a6c487381d50340b6
Jezreel Dwayne N. Ignacio <jnignacio.wup@gmail.com>
2024-03-18 08:53:17 +0800

    modified javascript

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ N = false,
 G = false,
 O = false;
 
-function newNumber (maxNumber) {
+function newNumber (maxNumber) {                                                                                                                                          
     // Addition = "+"
     // Subtraction = "-"
     // Multiplication = "*"
@@ -20,24 +20,36 @@ function newNumber (maxNumber) {
     if (x <= 15) {
         console.log(`New number is ${x} belongs to "B"`);
         B = true;
+        BNumber = x;
     } else if (x >= 16 && x <= 30) {
         console.log(`New number is ${x} belongs to "I"`);
         I = true;
+        INumber = x;
     } else if (x >= 31 && x <= 45) {
         console.log(`New number is ${x} belongs to "N"`);
         N = true;
+        NNumber = x;
     } else if (x >= 46 && x <= 60) {
         console.log(`New number is ${x} belongs to "G"`);
         G = true;
+        GNumber = x;
     } else if (x >= 61 && x <= 75) {
         console.log(`New number is ${x} belongs to "O"`);
         O = true;
+        ONumber = x;
     } else {
-        console.log(`New number ${x} is invalid.`);
+       // console.log(`New number ${x} is invalid.`);
     }
 
     if (B == true && I==true && N == true && G == true && O == true) {
-     console.log(`BINGOOOOOOO!!!`)   
+     console.log(`BINGOOOOOOO!!!`)
+     console.table({
+        B: BNumber,
+        I: INumber,
+        N: NNumber,
+        G: GNumber,
+        O: ONumber,
+     });   
     }
 
 
@@ -45,3 +57,20 @@ function newNumber (maxNumber) {
 }
 
 newNumber(75);
+
+// LOOPS
+let counter = 5;
+//for(counter; counter >= 0; counter -- ) {
+  // console.log(`Current counter is ${counter}`);
+   //if (counter == 2) break;
+
+   //newNumber(75); }
+
+//let counter = 10;
+//while(counter >= 0 ){
+  // console.log(`Current counter is ${counter}`);
+    //counter --; }
+
+while (B == false || I == false || N == false || G == false || O == false){
+    newNumber(75);
+}
